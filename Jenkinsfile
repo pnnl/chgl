de26125553d79a18254349f00b4a9458fc163313
Tim Stavenger <timothy.stavenger@pnnl.gov>
2018-05-29 11:43:31 -0700

    jenkins path wrangling

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ pipeline {
 
                     // SSH to puma.pnl.gov and execute jenkins-build.sh
                     //sh 'ssh jenkins@puma.pnl.gov workspace/jenkins-build.sh'
-                    sh 'ssh jenkins@puma.pnl.gov export CHPL_TEST_PERF_DIR=/home/jenkins/chgl-workspace/test/performance/dat && cd /home/jenkins/chgl-workspace/test/performance && start_test --performance -junit-xml -junit-xml-file /home/jenkins/chgl-workspace/test/performance/Logs/chapel-perf-tests.xml -numlocales 4'
+                    sh 'ssh jenkins@puma.pnl.gov "bash -s" < chdl-workspace/jenkins-build.sh'
 
                     // Get results back from puma.pnl.gov
                     //sh 'scp -r jenkins@puma.pnl.gov:workspace/ $WORKSPACE/'
--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export CHPL_TEST_PERF_DIR=workspace/test/performance/dat
-cd test/performance
-start_test --performance -junit-xml -junit-xml-file workspace/test/performance/Logs/chapel-perf-tests.xml -numlocales 4
+export CHPL_TEST_PERF_DIR=/home/jenkins/chgl-workspace/test/performance/dat
+cd /home/jenkins/chgl-test/performance
+start_test --performance -junit-xml -junit-xml-file /home/jenkins/chgl-workspace/test/performance/Logs/chapel-perf-tests.xml -numlocales 4
 
 # Generated HTML does not work locally or in Jenkins due to https://wiki.jenkins.io/display/JENKINS/Configuring+Content+Security+Policy. Copy files that use local resources instead.
 # cp -r ${WORKSPACE}/test/performance/html ${WORKSPACE}/test/performance/dat
