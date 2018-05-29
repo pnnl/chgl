e2616b40ff75a851db6a7c28dbae6aaf9d14e774
Tim Stavenger <timothy.stavenger@pnnl.gov>
2018-05-29 11:59:31 -0700

    correct path

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CHPL_TEST_PERF_DIR=/home/jenkins/chgl-workspace/test/performance/dat
-cd /home/jenkins/chgl-test/performance
+cd /home/jenkins/chgl-workspace/test/performance
 start_test --performance -junit-xml -junit-xml-file /home/jenkins/chgl-workspace/test/performance/Logs/chapel-perf-tests.xml -numlocales 4
 
 # Generated HTML does not work locally or in Jenkins due to https://wiki.jenkins.io/display/JENKINS/Configuring+Content+Security+Policy. Copy files that use local resources instead.
