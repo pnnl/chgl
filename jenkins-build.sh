0561e7141703d8da531257c705000a92e8cf23cf
Tim Stavenger <timothy.stavenger@pnnl.gov>
2018-05-29 12:22:22 -0700

    init chapel env

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# TODO parameterize Chapel install on puma.pnl.gov vs hard-coded path?
+cd /home/zale916/software/chapel/master-source
+source util/quickstart/setchplenv.bash
+
+# Execute peformance tests
 export CHPL_TEST_PERF_DIR=/home/jenkins/chgl-workspace/test/performance/dat
 cd /home/jenkins/chgl-workspace/test/performance
 start_test --performance -junit-xml -junit-xml-file /home/jenkins/chgl-workspace/test/performance/Logs/chapel-perf-tests.xml -numlocales 4
