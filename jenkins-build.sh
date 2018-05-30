dda62e05d74935bf9884fd750c622c4a577c882f
Tim Stavenger <timothy.stavenger@pnnl.gov>
2018-05-30 11:41:36 -0700

    disable unit tests

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -15,8 +15,8 @@ cd $CHAPEL_HOME
 source util/setchplenv.sh
 
 # Execute unit tests
-cd $WORKSPACE/test/unit
-start_test -junit-xml -junit-xml-file $WORKSPACE/test/unit/Logs/chapel-unit-tests.xml -numlocales 4
+#cd $WORKSPACE/test/unit
+#start_test -junit-xml -junit-xml-file $WORKSPACE/test/unit/Logs/chapel-unit-tests.xml -numlocales 4
 
 # Execute peformance tests
 export CHPL_TEST_PERF_DIR=$WORKSPACE/test/performance/dat
