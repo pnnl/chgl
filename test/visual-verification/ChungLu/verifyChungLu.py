476c7de4dd1c28f4ec5f11cc59ee45e8818a93dd
David Mentgen <Dam530@msstate.edu>
2018-06-13 21:35:12 -0500

    modified graphing for CL

--- test/visual-verification/ChungLu/verifyChungLu.py
+++ test/visual-verification/ChungLu/verifyChungLu.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as tkr
+import matplotlib.axes as axes
 import math
+import numpy as np
 
 with open("../../../data/output/ChungLu/condMat/ddChapelCL_E.csv", 'r') as f:
 	output_ddCL_E = f.read().splitlines()
@@ -35,31 +38,48 @@ output_e_y = []
 output_v_x = []
 output_v_y = []
 
-for i in range(len(output_ddCL_E)):
-	for j in range(len(output_ddCL_E[i])):
-		output_e_x.append(i+1)
-		output_e_y.append(output_mpdCL_E[i])
+#output_mpdCL_E.sort(reverse=True)
+#output_mpdCL_V.sort(reverse=True)
+#input_mpd_E.sort(reverse=True)
+#input_mpd_V.sort(reverse=True)
 
-for i in range(len(output_ddCL_V)):
-	for j in range(len(output_ddCL_V[i])):
-		output_v_x.append(i+1)
-		output_v_y.append(output_mpdCL_V[i])
+for i in range(len(output_mpdCL_E)):
+	#for j in range(len(output_ddCL_E[i])):
+	#	output_e_x.append(i+1)
+	#	output_e_y.append(output_mpdCL_E[i])
+    output_e_x.append(i+1)
+    output_e_y.append(float(output_mpdCL_E[i]))
 
-for i in range(len(input_dd_E)):
-	for j in range(len(input_dd_E[i])):
-		input_e_x.append(i+1)
-		input_e_y.append(input_mpd_E[i])
+for i in range(len(output_mpdCL_V)):
+	#for j in range(len(output_ddCL_V[i])):
+	#	output_v_x.append(i+1)
+	#	output_v_y.append(output_mpdCL_V[i])
+    output_v_x.append(i+1)
+    output_v_y.append(float(output_mpdCL_V[i]))
 
-for i in range(len(input_dd_V)):
-	for j in range(len(input_dd_V[i])):
-		input_v_x.append(i+1)
-		input_v_y.append(input_mpd_V[i])
+for i in range(len(input_mpd_E)):
+	#for j in range(len(input_dd_E[i])):
+	#	input_e_x.append(i+1)
+	#	input_e_y.append(input_mpd_E[i])
+    input_e_x.append(i+1)
+    input_e_y.append(float(input_mpd_E[i]))
 
-plt.loglog(output_e_x, output_e_y, marker = 'o', linestyle = '--', color = 'r', label='Output E')
-plt.loglog(output_v_x, output_v_y, marker = 'o', linestyle = '--', color='b', label='Output V')
-plt.loglog(input_e_x, input_e_y, marker = 'o', linestyle = '--', color='g', label='Input E')
-plt.loglog(input_v_x, input_v_x, marker = 'o', linestyle = '--', color='black', label='Input V')
+for i in range(len(input_mpd_V)):
+	#for j in range(len(input_dd_V[i])):
+	#	input_v_x.append(i+1)
+	#	input_v_y.append(input_mpd_V[i])
+    input_v_x.append(i+1)
+    input_v_y.append(float(input_mpd_V[i]))
+    #print(input_mpd_V[i])
 
+#plt.axis(xscale='log', yscale='log')
+
+#fig, ax = plt.subplots()
+
+plt.semilogx(output_e_x, output_e_y, marker = 'o', linestyle = '--', color = 'r', label='Output E')
+plt.semilogx(output_v_x, output_v_y, marker = 'o', linestyle = '--', color='b', label='Output V')
+plt.semilogx(input_e_x, input_e_y, marker = 'o', linestyle = '--', color='g', label='Input E')
+plt.semilogx(input_v_x, input_v_y, marker = 'o', linestyle = '--', color='black', label='Input V')
 plt.xlabel('Degree')
 plt.ylabel('PDMC')
 plt.title('Chung Lu Visual Verification')
