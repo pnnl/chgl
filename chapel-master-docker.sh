5b3c97b8f1c265cf75201eedb7100d369964b1a7
Tim Stavenger <timothy.stavenger@pnnl.gov>
2018-08-22 15:41:36 -0700

    new script to rebuild docker image

---
+++ chapel-master-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script uses a Dockerfile based on the master branch in https://github.com/tjstavenger-pnnl/chapel/blob/master/util/dockerfiles/master/
+# A pull request has been created to merge this back into Chapel. See https://github.com/chapel-lang/chapel/pull/10570
+
+mkdir docker-tmp
+cd docker-tmp
+wget https://raw.githubusercontent.com/tjstavenger-pnnl/chapel/master/util/dockerfiles/master/Dockerfile
+
+
+# login with user that has permissions to push to https://hub.docker.com/r/pnnl/chapel/
+sudo docker login
+
+# clean up all old docker images (be careful!)
+sudo docker system prune -a
+
+# build the image and push it to Docker Hub
+sudo docker build -t chapelmaster .
+sudo docker tag chapelmaster pnnl/chapel:master
+sudo docker push pnnl/chapel:master
+
+cd ..
+rm -rf docker-tmp
+
+
