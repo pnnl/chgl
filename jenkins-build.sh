71b7aacf9d0d17f7ddcf038e4e815265e98f05d0
Tim Stavenger <timothy.stavenger@pnnl.gov>
2019-05-01 07:31:10 -0700

    add prereq modules

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -8,6 +8,10 @@ WORKSPACE=/lustre/jenkins/chgl-workspace
 
 # Load required modules
 export MODULEPATH=/home/zale916/software/modules:$MODULEPATH
+module load gcc/8.2.0
+module load openmpi/3.1.3
+module load hdf5/1.10.5
+module load zmq/4.3.1
 module load chapel/1.19.0
 
 # Initialize Chapel environment
