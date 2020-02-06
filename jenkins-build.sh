9cfcc8b7dac73c60bb82fb0a97d32f3625001b32
Jesun Sahariar Firoz <jesun.firoz@pnnl.gov>
2020-02-05 19:27:15 -0800

    Updating Jenkins script

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -6,12 +6,11 @@
 WORKSPACE=/lustre/jenkins/chgl-workspace
 
 # Load required modules
-export MODULEPATH=/home/zale916/software/modules:$MODULEPATH
+export MODULEPATH=/home/firo017/softwares/modules:$MODULEPATH
 module load gcc/8.2.0
 module load openmpi/3.1.3
-module load hdf5/1.10.5
-module load zmq/4.3.1
-module load chapel/1.19.0
+module load clang/8.0.1 
+module load chapel/1.20.0
 
 # Execute peformance tests
 export CHPL_TEST_LAUNCHCMD="$CHPL_HOME/util/test/chpl_launchcmd.py --CHPL_LAUNCHCMD_DEBUG"
